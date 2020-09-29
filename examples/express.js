d3992d369ef6cf8d4ff3bf63f15942c4bc3ffccd
Miguel Martinez <miguel.martinez.inf@gmail.com>
2020-09-29 23:32:06 +0200

    feat: refactored library due to issues with next tick

--- examples/express.js
+++ examples/express.js
@@ -1,24 +1,22 @@
-
 var inbound = require('../index'),
-    express = require('express');
+  express = require('express')
 
-var app = express();
+var app = express()
 
 app.use(function (req, res, next) {
-  var referrer = req.header('referrer');
-  var href = req.url;
-  inbound.referrer.parse(href, referrer, function (err, desc) {
-    req.referrer = desc;
-    next(err);
-  });
-});
+  var referrer = req.header('referrer')
+  var href = req.url
+  const desc = inbound.referrer.parse(href, referrer)
+  req.referrer = desc
+  next(err)
+})
 
-app.use(app.router);
+app.use(app.router)
 
 app.get('/', function (req, res, next) {
-  return res.send(req.referrer);
-});
+  return res.send(req.referrer)
+})
 
-var port = 8000;
-app.listen(port);
-console.log('Server listening on port : ' + port);
+var port = 8000
+app.listen(port)
+console.log('Server listening on port : ' + port)
